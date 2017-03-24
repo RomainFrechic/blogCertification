c96c73f3b7085b87d8d11e9bfe7aacc0b8708011
Romain <RomainFrechic@outlook.fr>
2017-03-24 15:40:29 +0100

    create in views folder layouts and posts, as well as create in public bootstrap.css and bootstrap.js

--- app/Categories.php
+++ app/Categories.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     protected $guarded = ['id','created_at'];
+
+    public function posts(){
+    	return $this->hasMany('Post');
+    }
 }
--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $guarded = ['id','created_at'];
+
+    public function post(){
+    	return $this->belongsTo('Post');
+    }
+
+    public function user(){
+    	return $this->belongsTo('User');
+    }
 }
